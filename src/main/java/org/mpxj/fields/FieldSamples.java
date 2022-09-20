ea0b116ece0e43bb146ac46f037ddef3cd3f615e
joniles <jon.iles@bcs.org.uk>
2022-09-20 19:53:08 +0100

    WIP

--- src/main/java/org/mpxj/fields/FieldSamples.java
+++ src/main/java/org/mpxj/fields/FieldSamples.java
@@ -1,42 +1,22 @@
 package org.mpxj.fields;
 
-import net.sf.mpxj.ProjectFile;
-import net.sf.mpxj.Task;
-import net.sf.mpxj.TaskField;
-import org.mpxj.calendars.CalendarSamples;
+import net.sf.mpxj.*;
+import net.sf.mpxj.reader.UniversalProjectReader;
 
 import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.Set;
 
 public class FieldSamples {
 
     public static void main(String[] argv) throws Exception {
         FieldSamples samples = new FieldSamples();
-        samples.dateTest();
         //samples.basicOperations();
-    }
-
-    private void dateTest() throws Exception
-    {
-        SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-        Date date = new java.util.Date(119, 11, 31, 1, 0);
-        System.out.println(date.toString());
-        System.out.println(dateFormat.format(date));
-        System.out.println();
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(2019, 11, 31, 1, 0, 0);
-        date = cal.getTime();
-        System.out.println(date.toString());
-        System.out.println(dateFormat.format(date));
-        System.out.println();
-
-        date = dateFormat.parse("2019-12-31 01:00:00");
-        System.out.println(date.toString());
-        System.out.println(dateFormat.format(date));
+        //samples.startVariance();
+        samples.fieldType();
     }
 
     private void basicOperations() throws Exception
@@ -49,7 +29,7 @@ public class FieldSamples {
         task.setName("Task 1");
 
         String name = task.getName();
-        System.out.println("Task name:" + name);
+        System.out.println("Task name: " + name);
 
         // Set the start date
         DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
@@ -61,13 +41,144 @@ public class FieldSamples {
         task = file.addTask();
         task.set(TaskField.NAME, "Task 2");
 
-        name = (String)task.getCurrentValue(TaskField.NAME);
-        System.out.println("Task name:" + name);
+        name = (String)task.get(TaskField.NAME);
+        System.out.println("Task name: " + name);
 
         startDate = df.parse("11/05/2022");
         task.set(TaskField.START, startDate);
 
         System.out.println("Start date: " + df.format(task.getStart()));
+    }
 
+    private void startVariance() throws Exception
+    {
+        // Set up the sample project
+        ProjectFile file = new ProjectFile();
+
+        // We need at least a default calendar to calculate variance
+        file.setDefaultCalendar(file.addDefaultBaseCalendar());
+
+        // Create tasks
+        Task task1 = file.addTask();
+        Task task2 = file.addTask();
+
+        // Set up example dates
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        Date baselineStart = df.parse("01/05/2022");
+        Date startDate = df.parse("10/05/2022");
+
+        // Update task1 using methods
+        task1.setStart(startDate);
+        task1.setBaselineStart(baselineStart);
+
+        // Update task2 using TaskField enumeration
+        task2.set(TaskField.START, startDate);
+        task2.set(TaskField.BASELINE_START, baselineStart);
+
+        // Show the variance being retrieved by method and TaskField enumeration
+        System.out.println("Task 1");
+        System.out.println("Start Variance from method: " + task1.getStartVariance());
+        System.out.println("Start Variance from get: " + task1.get(TaskField.START_VARIANCE));
+        System.out.println();
+
+        System.out.println("Task 2");
+        System.out.println("Start Variance from method: " + task2.getStartVariance());
+        System.out.println("Start Variance from get: " + task2.get(TaskField.START_VARIANCE));
+        System.out.println();
+
+        // Use the getCachedValue method
+        System.out.println("Task1 Start Variance cached value: " + task1.getCachedValue(TaskField.START_VARIANCE));
+        System.out.println("Task2 Start Variance cached value: " + task2.getCachedValue(TaskField.START_VARIANCE));
+
+        /// Clear the cached start variance by updating one of its dependencies
+        baselineStart = df.parse("03/05/2022");
+        task1.setBaselineStart(baselineStart);
+
+        // Explicitly clear the start variance
+        task2.setStartVariance(null);
+
+        // We no longer have cached values
+        System.out.println("Task1 Start Variance: " + task1.getCachedValue(TaskField.START_VARIANCE));
+        System.out.println("Task2 Start Variance: " + task2.getCachedValue(TaskField.START_VARIANCE));
+        System.out.println();
+
+        System.out.println("Task1 Start Variance from method: " + task1.getStartVariance());
+        System.out.println("Task2 Start Variance from method: " + task2.getStartVariance());
+        System.out.println();
+
+        System.out.println("Task1 Start Variance cached value: " + task1.getCachedValue(TaskField.START_VARIANCE));
+        System.out.println("Task2 Start Variance cached value: " + task2.getCachedValue(TaskField.START_VARIANCE));
+    }
+
+    private void indexedFields()
+    {
+        ProjectFile file = new ProjectFile();
+        Task task = file.addTask();
+
+        task.setText(1, "This is Text 1");
+        String text1 = task.getText(1);
+        System.out.println("Text 1 is: " + text1);
+
+        task.set(TaskField.TEXT1, "This is also Text 1");
+        text1 = (String)task.get(TaskField.TEXT1);
+        System.out.println("Text 1 is: " + text1);
+    }
+
+    private void populatedFields() throws Exception
+    {
+        ProjectFile file = new UniversalProjectReader().read("example.mpp");
+        Set<ProjectField> populatedProjectFields = file.getProjectProperties().getPopulatedFields();
+        Set<TaskField> populatedTaskFields = file.getTasks().getPopulatedFields();
+        Set<ResourceField> populatedResourceFields = file.getResources().getPopulatedFields();
+        Set<AssignmentField> populatedAssignmentFields = file.getResourceAssignments().getPopulatedFields();
+    }
+
+    private void fieldType()
+    {
+        FieldType type = TaskField.START_VARIANCE;
+        System.out.println("name(): " + type.name());
+        System.out.println("getName(): " + type.getName());
+        System.out.println("getFieldTypeClass(): " + type.getFieldTypeClass());
+        System.out.println("getDataType(): " + type.getDataType());
+    }
+
+
+    private String getStringValue(FieldContainer container, FieldType type)
+    {
+        Object value = container.get(type);
+        if (value == null)
+        {
+            return "";
+        }
+
+        String result;
+        switch (type.getDataType())
+        {
+            case CURRENCY:
+            {
+                result = new DecimalFormat("£0.00").format((Number)value);
+                break;
+            }
+
+            case DATE:
+            {
+                result = new SimpleDateFormat("dd/MM/yyyy").format((Date)value);
+                break;
+            }
+
+            case BOOLEAN:
+            {
+                result = ((Boolean)value).booleanValue() ? "Yes" : "No";
+                break;
+            }
+
+            default:
+            {
+                result = value.toString();
+                break;
+            }
+        }
+
+        return result;
     }
 }
