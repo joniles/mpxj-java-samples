99442196301a8afcbfc476dfbf9109de7b89f316
joniles <jon.iles@bcs.org.uk>
2023-05-05 11:10:41 +0100

    WIP

--- src/main/java/org/mpxj/external/ExternalSamples.java
+++ src/main/java/org/mpxj/external/ExternalSamples.java
@@ -1,27 +1,70 @@
 package org.mpxj.external;
 
+import net.sf.mpxj.MPXJException;
 import net.sf.mpxj.ProjectFile;
 import net.sf.mpxj.Task;
 import net.sf.mpxj.reader.UniversalProjectReader;
 
+import java.io.File;
+
 public class ExternalSamples {
     public static void main(String[] argv) throws Exception {
         ExternalSamples samples = new ExternalSamples();
-        samples.subprojects();
+        samples.identifyExternalProjects();
+        samples.openingExternalProjects();
     }
 
-    private void subprojects() throws Exception
+    private void identifyExternalProjects() throws Exception
     {
-ProjectFile file = new UniversalProjectReader().read("sample.mpp");
-for (Task task : file.getTasks())
-{
-    if (task.getExternalProject())
-    {
-        System.out.println(task.getName() + " is a subproject");
-        System.out.println("The path to the file is: " + task.getSubprojectFile());
-        System.out.println("The GUID of this project is: " + task.getSubprojectGUID());
-        System.out.println("The offset used when displaying Unique ID values is: " + task.getSubprojectTasksUniqueIDOffset());
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        for (Task task : file.getTasks())
+        {
+            if (task.getExternalProject())
+            {
+                System.out.println(task.getName() + " is a subproject");
+                System.out.println("The path to the file is: " + task.getSubprojectFile());
+                System.out.println("The GUID of this project is: " + task.getSubprojectGUID());
+                System.out.println("The offset used when displaying Unique ID values is: " + task.getSubprojectTasksUniqueIDOffset());
+            }
+        }
     }
-}
+
+    private void openingExternalProjects() throws Exception {
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        Task externalProject = file.getTaskByID(Integer.valueOf(1));
+        String filePath = externalProject.getSubprojectFile();
+        ProjectFile externalProjectFile = new UniversalProjectReader().read(filePath);
     }
+
+    private void openingExternalProjectsUsingMpxj() throws Exception {
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        Task externalProjectTask = file.getTaskByID(Integer.valueOf(1));
+        ProjectFile externalProjectFile = externalProjectTask.getSubprojectObject();
+    }
+
+    private void openingExternalProjectsUsingMpxjWithWorkingDir() throws Exception {
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        file.setSubprojectWorkingDirectory(new File("/path/to/directory"));
+        Task externalProjectTask = file.getTaskByID(Integer.valueOf(1));
+        ProjectFile externalProjectFile = externalProjectTask.getSubprojectObject();
+    }
+
+    private void expandExternalProjectTask() throws Exception {
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        Task externalProjectTask = file.getTaskByID(Integer.valueOf(1));
+        System.out.println("Task has child tasks: " + externalProjectTask.hasChildTasks());
+        externalProjectTask.expandSubproject();
+        System.out.println("Task has child tasks: " + externalProjectTask.hasChildTasks());
+    }
+
+    private void expandAllExternalProjects() throws Exception {
+        ProjectFile file = new UniversalProjectReader().read("sample.mpp");
+        file.expandSubprojects();
+
+        Task externalProjectTask = file.getTaskByID(Integer.valueOf(1));
+        System.out.println("Task has child tasks: " + externalProjectTask.hasChildTasks());
+        externalProjectTask.expandSubproject();
+        System.out.println("Task has child tasks: " + externalProjectTask.hasChildTasks());
+    }
+
 }
