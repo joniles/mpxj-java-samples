bb3315c051d7f7541a00e5428fa291cf6d4ed79b
joniles <jon.iles@bcs.org.uk>
2024-06-28 09:51:05 +0100

    Update samples

--- src/main/java/org/mpxj/howto/read/PMXMLBaselines.java
+++ src/main/java/org/mpxj/howto/read/PMXMLBaselines.java
@@ -4,8 +4,6 @@ import net.sf.mpxj.ProjectFile;
 import net.sf.mpxj.primavera.PrimaveraPMFileReader;
 import net.sf.mpxj.primavera.PrimaveraBaselineStrategy;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.List;
 
 public class PMXMLBaselines
@@ -14,7 +12,6 @@ public class PMXMLBaselines
    {
       PrimaveraPMFileReader reader = new PrimaveraPMFileReader();
       reader.setBaselineStrategy(PrimaveraBaselineStrategy.CURRENT_ATTRIBUTES);
-      InputStream is = new FileInputStream("my-sample.xml");
-      List<ProjectFile> files = reader.readAll(is);
+      List<ProjectFile> files = reader.readAll("my-sample.xml");
    }
 }
--- src/main/java/org/mpxj/howto/read/PMXMLLinkCrossProject.java
+++ src/main/java/org/mpxj/howto/read/PMXMLLinkCrossProject.java
@@ -3,8 +3,6 @@ package org.mpxj.howto.read;
 import net.sf.mpxj.ProjectFile;
 import net.sf.mpxj.primavera.PrimaveraPMFileReader;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.List;
 
 public class PMXMLLinkCrossProject
@@ -13,7 +11,6 @@ public class PMXMLLinkCrossProject
    {
       PrimaveraPMFileReader reader = new PrimaveraPMFileReader();
       reader.setLinkCrossProjectRelations(true);
-      InputStream is = new FileInputStream("my-sample.xml");
-      List<ProjectFile> files = reader.readAll(is);
+      List<ProjectFile> files = reader.readAll("my-sample.xml");
    }
 }
