f897cfa0b0e2fd6c6ad877e26614b62611538885
joniles <jon.iles@bcs.org.uk>
2024-06-28 09:34:28 +0100

    Update sample

--- src/main/java/org/mpxj/howto/read/PMXMLReadAll.java
+++ src/main/java/org/mpxj/howto/read/PMXMLReadAll.java
@@ -3,8 +3,6 @@ package org.mpxj.howto.read;
 import net.sf.mpxj.ProjectFile;
 import net.sf.mpxj.primavera.PrimaveraPMFileReader;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.List;
 
 public class PMXMLReadAll
@@ -12,7 +10,6 @@ public class PMXMLReadAll
    public void read() throws Exception
    {
       PrimaveraPMFileReader reader = new PrimaveraPMFileReader();
-      InputStream is = new FileInputStream("my-sample.xml");
-      List<ProjectFile> files = reader.readAll(is);
+      List<ProjectFile> files = reader.readAll("my-sample.xml");
    }
 }
