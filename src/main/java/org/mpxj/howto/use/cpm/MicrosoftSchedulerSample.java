a69a2bd2205c7e5fcad7e8a3f128e2d0239c3164
joniles <jon@timephased.com>
2025-04-04 08:52:55 +0100

    WIP

--- src/main/java/org/mpxj/howto/use/cpm/MicrosoftSchedulerSample.java
+++ src/main/java/org/mpxj/howto/use/cpm/MicrosoftSchedulerSample.java
@@ -24,30 +24,36 @@ public class MicrosoftSchedulerSample
       ProjectCalendar calendar = file.addDefaultBaseCalendar();
       file.setDefaultCalendar(calendar);
 
-      Task task1 = file.addTask();
+      Task summary1 = file.addTask();
+      summary1.setName("Summary 1");
+
+      Task task1 = summary1.addTask();
       task1.setName("Task 1");
       task1.setDuration(Duration.getInstance(3, TimeUnit.DAYS));
 
-      Task task2 = file.addTask();
+      Task task2 = summary1.addTask();
       task2.setName("Task 2");
       task2.setDuration(Duration.getInstance(2, TimeUnit.DAYS));
 
-      Task task3 = file.addTask();
+      Task task3 = summary1.addTask();
       task3.setName("Task 3");
       task3.setDuration(Duration.getInstance(5, TimeUnit.DAYS));
 
       task3.addPredecessor(new Relation.Builder().predecessorTask(task1));
       task3.addPredecessor(new Relation.Builder().predecessorTask(task2));
 
-      Task task4 = file.addTask();
+      Task summary2 = file.addTask();
+      summary2.setName("Summary 2");
+
+      Task task4 = summary2.addTask();
       task4.setName("Task 4");
       task4.setDuration(Duration.getInstance(2, TimeUnit.DAYS));
 
-      Task task5 = file.addTask();
+      Task task5 = summary2.addTask();
       task5.setName("Task 5");
       task5.setDuration(Duration.getInstance(2, TimeUnit.DAYS));
 
-      Task task6 = file.addTask();
+      Task task6 = summary2.addTask();
       task6.setName("Task 6");
       task6.setDuration(Duration.getInstance(2, TimeUnit.DAYS));
 
@@ -64,22 +70,22 @@ public class MicrosoftSchedulerSample
       new MicrosoftScheduler().schedule(file, LocalDateTime.of(2025, 4, 11, 8, 0));
 
       System.out.println(writeTaskHeaders());
-      file.getTasks().stream().forEach(t -> System.out.println(writeTaskData(t)));
+      file.getTasks().forEach(t -> System.out.println(writeTaskData(t)));
 
       new UniversalProjectWriter(FileFormat.MSPDI).write(file, filename);
    }
 
    private String writeTaskHeaders()
    {
-      return TASK_COLUMNS.stream().map(c -> c.toString()).collect(Collectors.joining("\t"));
+      return TASK_COLUMNS.stream().map(TaskField::toString).collect(Collectors.joining("\t"));
    }
 
    private String writeTaskData(Task task)
    {
-      return TASK_COLUMNS.stream().map(c -> task.get(c).toString()).collect(Collectors.joining("\t"));
+      return TASK_COLUMNS.stream().map(c -> String.valueOf(task.get(c))).collect(Collectors.joining("\t"));
    }
 
-   private static final List<TaskField> TASK_COLUMNS = Arrays.asList(new TaskField[]{
+   private static final List<TaskField> TASK_COLUMNS = Arrays.asList(
       TaskField.ID,
       TaskField.UNIQUE_ID,
       TaskField.NAME,
@@ -90,6 +96,5 @@ public class MicrosoftSchedulerSample
       TaskField.LATE_START,
       TaskField.LATE_FINISH,
       TaskField.TOTAL_SLACK,
-      TaskField.CRITICAL
-   });
+      TaskField.CRITICAL);
 }
